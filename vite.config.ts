dcc751f8a351b7dece6738a98f889e281bb265be
prolly_jesse <106178008+prolly-jesse@users.noreply.github.com>
2025-11-24 13:20:21 +0300

    vite configure

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base : process.env.VITE_BASE_PATH || "/theekulture" ,
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
